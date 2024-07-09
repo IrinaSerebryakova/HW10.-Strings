ada4ea56e7f906e6435d2ecc204284e0b5d552b3
Iserebryakova <ia.serebr@gmail.com>
2024-07-09 21:08:17 +0300

    Проверьте, пожалуйста, дз №10 по теме Строки.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 
 public class Main {
     public static void main(String[] args) {
+
         //task 1
         String firstName = "Ivan";
         String middleName = "Ivanovich";
@@ -8,10 +9,12 @@ public class Main {
         String fullName = lastName + " " + firstName + " " + middleName;
         System.out.printf( "ФИО сотрудника - %s.\n", fullName);
 
-//task 2
+
+        //task 2
         System.out.printf("Данные ФИО сотрудника для заполнения отчета - %s.\n", fullName.toUpperCase());
 
-//task 3
+
+        //task 3
         String fullName1 = "Иванов Семён Семёнович";
         String fullNameRefactor = fullName1.replace("ё","e");
         System.out.printf("Данные ФИО сотрудника – %s.\n", fullNameRefactor);
